9249fc4621ba09afd3dc8e598bc80e0a688463d7
raf555 <irsoekarno888@gmail.com>
2020-08-01 09:34:41 +0700

    Inisialisasi node.js -Rezda

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('test')
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "Public/landingpage.html")); // BUAT AKSES FRONT END
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});

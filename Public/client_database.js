f3f7a5a71268dd9ad2d3c6751875856d8b36c889
azharfatrr <m.azhar.faturahman@gmail.com>
2020-08-04 01:05:30 +0700

    memperbaiki tambahBuku.html

--- Public/client_database.js
+++ Public/client_database.js
@@ -2,7 +2,10 @@
 const cari = document.getElementById("cariData");
 cari.addEventListener('click', async event => {
     const judul = document.getElementById("judul");
-    console.log('klik');
+    const found = document.getElementById('found');
+    let listBuku = document.getElementById('listBuku');
+
+    // URL dan Penambahan Parameter
     let url = new URL('https://www.googleapis.com/books/v1/volumes');
     params = {q:`${judul.value}`,maxResults:40,orderBy:'relevance'};
     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
@@ -11,9 +14,8 @@ cari.addEventListener('click', async event => {
     console.log(response);
     const data = await response.json();
     const daftarBuku = data.items
-    const found = document.getElementById('found')
+    
     // Menampilkan buku
-    let listBuku = document.getElementById('listBuku')
     console.log(listBuku);
     let output = '<h3>Menampilkan Pencarian</h3>'
     for (let i = 0; i < daftarBuku.length; i++) {
@@ -23,16 +25,20 @@ cari.addEventListener('click', async event => {
             tampil(imgsrc);
         }
         catch (error) {
-            const imgsrc = '../Database/img/404.jpg';
+            const imgsrc = 'none.jpg';
             tampil(imgsrc);
-        }
+        }   
         function tampil(imgsrc) {
             //console.log(imgsrc);
             found.innerHTML = `
-            <p>Menampilkan ${daftarBuku.length} hasil`;
+            <p>Terdapat ${daftarBuku.length} hasil`;
             output += `
-            <div id='buku'>
-                <img src=${imgsrc} width='256px' height='394px'>
+                <div class='buku'>
+                    <img class='cover' src=${imgsrc} width='256px' height='394px'>
+                <div class='keterangan'>
+                    <p>Judul : ${buku.title}</p>
+                    <p>Pengarang : ${buku.authors} </p>
+                </div>
             </div>`;
             listBuku.innerHTML = output
         }

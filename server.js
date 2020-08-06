b7b4d59b2ce4f773d3c74056ea9ab2244bea0798
azharfatrr <m.azhar.faturahman@gmail.com>
2020-08-06 21:19:16 +0700

    bikin API query buku dan genre

--- server.js
+++ server.js
@@ -1,36 +1,31 @@
+// Inisialisasi
 const express = require("express");
 const Datastore = require('nedb');
-
 const app = express();
+const { request, response, query } = require("express");
+
+// Buka database
 const dataBuku = new Datastore('Database/data_buku.db');
 dataBuku.loadDatabase();
-
+// Inisialisasi server
 app.use(express.static(__dirname + "/Public/")); // BUAT AKSES FRONT END
 app.use(express.json({ limit: '10mb' }));
-
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/Public/landingpage.html"); // INDEKS FRONT END
-});
-
 const listener = app.listen(3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
-app.get("/api", (request, response) => {
-  dataBuku.find({}, (err, data) => {
-    if (err) {
-      response.end();
-      return;
-    }
-    response.json(data);
-  });
+// Buka landpage
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/Public/landingpage.html"); // INDEKS FRONT END
 });
 
+// Buka page tambah buku
 app.get("/tambahBuku", (request, response) => {
   response.sendFile(__dirname + "/Public/tambahBuku.html");
   console.log("opening tambahBuku.html");//response.json("lancar")
 });
 
+// API nambah buku
 app.post("/tambahBuku", (request, response) => {
   const data = request.body;
   const isbn = data.isbn
@@ -50,5 +45,59 @@ app.post("/tambahBuku", (request, response) => {
       response.json({text : "Gagal: Buku sudah ada!"});
     }
   })
-  //dataBuku.find({judul : /Lord/i}, (err, docs)=> {})
+})
+
+// Dummy page cari buku
+app.get("/cariBuku", (request, response) => {
+  response.sendFile(__dirname + "/Public/dummy_caribuku.html"); // INDEKS FRONT END
+});
+// Dummy cari genre
+app.get("/cariGenre", (request, response) => {
+  response.sendFile(__dirname + "/Public/dummy_genre.html"); // INDEKS FRONT END
+});
+
+// API buka semua database
+app.get("/api", (request, response) => {
+  dataBuku.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.json(data);
+  });
+});
+
+// API query data buku
+app.get('/api/cariBuku/:query', (request,response) => {
+  console.log(`Request data masuk: ${request.params.query}`);
+  // Ubah parameter query jadi regex
+  const query = new RegExp(request.params.query,'i');
+  // Cari buku di database berdasarkan judul dan pengarang
+  dataBuku.find({ $or: [{ judul: query }, { pengarang: query }] }, (err, data) => {
+    if (err) { // Error Handling
+      console.log('there is an error in database query');
+      console.log(err);
+      response.end()
+    }
+    else { // Kembalikan hasil
+      response.json(data)
+    }
+  })
+})
+
+// API genre
+app.get('/api/cariGenre/:genre', (request,response) => {
+  console.log(`Request genre masuk: ${request.params.genre}`);
+  genre = request.params.genre 
+  // Cari buku di database berdasarkan genre
+  dataBuku.find({genre : genre}, (err, data) => {
+    if (err) { // Error Handling
+      console.log('there is an error in database query');
+      console.log(err);
+      response.end()
+    }
+    else { // Kembalikan hasil
+      response.json(data)
+    }
+  })
 })

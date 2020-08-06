4d6ea2ddbab3928a3f478edfa7196c3fba004d73
azharfatrr <m.azhar.faturahman@gmail.com>
2020-08-06 16:55:38 +0700

    push yang rifky minta

--- Public/src/main.js
+++ Public/src/main.js
@@ -1,23 +1,24 @@
 const welcomeBook = document.getElementById("welcome-book");
 
-const getData = async () => {
+async function getData() {
   // Ini yang ngikutin syntax
-  // const response = await fetch('/api', { method: 'GET' });
-  // const jsonResponse = await response.json();
-
-  // Ini yang ditambah error handling
-  // try {
-  //   const response = await fetch('/api');
-  //   if (response.ok) {
-  //     const jsonResponse = await response.json();
-  //     return jsonResponse;
-  //   }
-  //   throw new Error('Request failed!');
-  // } catch (error) {
-  //   console.log(error);
-  // };
+  const response = await fetch('/api', { method: 'GET' });
+  const jsonResponse = await response.json();
+  console.log(jsonResponse);
 }
+  // Ini yang ditambah error handling
+//   try {
+//     const response = await fetch('/api');
+//     if (response.ok) {
+//       const jsonResponse = await response.json();
+//       return jsonResponse;
+//     }
+//     throw new Error('Request failed!');
+//   } catch (error) {
+//     console.log(error);
+//   };
+// }
 
-getData;
+getData();
 // console.log(data);
 //welcomeBook.innerHTML = `<div class='col-md-4 col-xs-12 padding text-center'><img src=${data[0].img}><h3 class="text-center">${data[0].judul}</h3><p class="text-center">${data[0].deskripsi}</p></div>`;

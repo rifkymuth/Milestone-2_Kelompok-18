dff8e019619a288ee0bc39883d16385dc6b2100a
rifkymuth <rifkyvirtualacc@gmail.com>
2020-08-08 17:30:37 +0700

    styling carousel + ngambil data dari server

--- Public/src/main.js
+++ Public/src/main.js
@@ -3,7 +3,6 @@ var welcomeBook = document.getElementById("welcome-book");
 async function getData() {
   const response = await fetch("/api", { method: "GET" });
   const data = await response.json();
-  //welcomeBook.innerHTML = `<div class='col-md-4 col-xs-12 padding text-center'><img src=${data[0].img}><h3 class="text-center">${data[0].judul}</h3><p class="text-center">${data[0].deskripsi}</p></div>`;
 }
 
 getData();
